426e54607d7a04ea0a795cc042f56f8fa83c0940
Murilo Thom <mthom@gvdasa.com.br>
2025-06-24 18:47:01 -0300

    fix: remove footer temporarily

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,23 +2,23 @@
 import React from 'react';
 import { Box, Typography, Container } from '@mui/material';
 
-const Footer: React.FC = () => (
-  <Box
-    component="footer"
-    sx={{
-      py: 2,
-      mt: 'auto',
-      backgroundColor: (theme) =>
-        theme.palette.mode === 'light' ? theme.palette.grey[100] : theme.palette.grey[900],
-    }}
-  >
-    <Container maxWidth="lg" sx={{ textAlign: 'center' }}>
-      <Typography variant="body2" color="text.secondary" fontWeight={300}>
-        © {new Date().getFullYear()} Down
-        <span style={{ fontWeight: 400 }}>SC</span>. <strong>(not)</strong> all rights reserved.
-      </Typography>
-    </Container>
-  </Box>
-);
+const Footer: React.FC = () =>
+  // <Box
+  //   component="footer"
+  //   sx={{
+  //     py: 2,
+  //     mt: 'auto',
+  //     backgroundColor: (theme) =>
+  //       theme.palette.mode === 'light' ? theme.palette.grey[100] : theme.palette.grey[900],
+  //   }}
+  // >
+  //   <Container maxWidth="lg" sx={{ textAlign: 'center' }}>
+  //     <Typography variant="body2" color="text.secondary" fontWeight={300}>
+  //       © {new Date().getFullYear()} Down
+  //       <span style={{ fontWeight: 400 }}>SC</span>. <strong>(not)</strong> all rights reserved.
+  //     </Typography>
+  //   </Container>
+  // </Box>
+  null;
 
 export default Footer;
