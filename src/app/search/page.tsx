11912d569f6b5faaddb116ca6afbc25b073bcbe5
Murilo Thom <mthom@gvdasa.com.br>
2025-07-24 17:35:32 -0300

    fix: remove ads

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -5,7 +5,6 @@ import { Container, CircularProgress, Alert, Box } from '@mui/material';
 import { SearchBar } from '@/components/SearchBar';
 import { useSearchTracks } from '@/hooks/useSearchTracks';
 import { TrackCard } from '@/components/TrackCard';
-import { GoogleAdSense } from '@/components/GoogleAdSense';
 import { env } from '@/env/env';
 
 export default function SearchPage() {
@@ -18,21 +17,6 @@ export default function SearchPage() {
   return (
     <Container sx={{ py: 4 }}>
       <SearchBar onSearch={handleSearch} />
-      <Box mt={2} mb={2}>
-        <GoogleAdSense
-          dataAdFormat="auto"
-          dataAdFullWidthResponsive
-          dataAdSlot={env.ADSENSE_SLOT_1}
-        />
-      </Box>
-      <Box mt={2} mb={2}>
-        <GoogleAdSense
-          dataAdFormat="auto"
-          dataAdFullWidthResponsive
-          dataAdSlot={env.ADSENSE_SLOT_2}
-        />
-      </Box>
-
       <Box mt={4}>
         {loading && (
           <Box display="flex" justifyContent="center" my={2}>
